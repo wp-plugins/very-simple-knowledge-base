5c35aa6b752a73d60758c3e8ca9ed406977fbcd7
Guido07111975 <Guido07111975@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-29 20:06:37 +0000

    First version
    git-svn-id: https://plugins.svn.wordpress.org/very-simple-knowledge-base/trunk@975470 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Very Simple Knowledge Base ===
+Contributors: Guido07111975
+Version: 1.0
+License: GNU General Public License v3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Requires at Least: 3.7
+Tested up to: 3.9.2
+Stable tag: trunk
+Tags: simple, knowledge, base, bulletin, board, faq, wiki, portal
+
+
+== Changelog == 
+Version 1.0
+- first stable release
+
+
+== DESCRIPTION ==
+This is a very simple plugin to create a Knowledge Base, Bulletin Board, FAQ, Wiki or Weblink Portal. It uses the default WP categories and posts. Use shortcode [knowledgebase] to display your categories and posts in 4 columns. For more info please check readme file.
+
+
+== INSTALLATION == 
+After installation create a page and add shortcode [knowledgebase]. Now this page will display your categories and posts in 4 columns.
+
+
+== Screenshots == 
+1. Very Simple Knowledge Base in Twenty Twelve theme.
+
+
+== OTHER NOTES ==
+This plugin is translation-ready (Dutch translation included). More translations are very welcome! Please send them to: info@guidovanderleest.nl
+You can translate this into your own language using for example plugin Codestyling Localization: http://wordpress.org/plugins/codestyling-localization/
+
+
+== CREDITS ==
+Without the WordPress codex and help from the WordPress community I was not able to develop this plugin, so: thank you!
+Special thanks to Niels van Renselaar for helping me with the shortcode.
+
+Enjoy,
+Guido
---
+++ vskb.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Plugin Name: Very Simple Knowledge Base
+ * Description: This is a very simple plugin to create a knowledgebase. Use shortcode [knowledgebase] to display your categories and posts in 4 columns. For more info please check readme file.
+ * Version: 1.0
+ * Author: Guido van der Leest
+ * Author URI: http://www.guidovanderleest.nl
+ * License: GNU General Public License v3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: vskb
+ * Domain Path: translation
+ */
+
+// Load the plugin's text domain
+function vskb_init() { 
+	load_plugin_textdomain( 'vskb', false, dirname( plugin_basename( __FILE__ ) ) . '/translation' );
+}
+add_action('plugins_loaded', 'vskb_init');
+ 
+
+// Enqueues plugin scripts
+function vskb_scripts() {	
+	if(!is_admin())
+	{
+		wp_enqueue_style('vskb_style', plugins_url('vskb_style.css',__FILE__));
+	}
+}
+add_action('wp_enqueue_scripts', 'vskb_scripts');
+
+
+// Creating the shortcode
+function knowledgebase_function() {
+
+	$return = "";
+
+	$return .= '<div id="vskb">';
+
+	$cats = get_categories('hide_empty=0&orderby=name&order=asc');
+
+	foreach ($cats as $cat) :
+
+		$return .= '<ul class="vskb-cat-list"><li class="vskb-cat-name"><a href="'. get_category_link( $cat->term_id ) .'" title="'. $cat->name .'" >'. $cat->name .'</a></li>';
+
+		$args = array(
+			'posts_per_page' => -1, // max number of post per category
+			'cat' => $cat->term_id
+		);
+
+		$posts = get_posts($args); 
+
+		if ( count($posts) > 0 ) :
+
+		foreach( $posts AS $single_post ) :
+	
+			$return .=  '<li class="vskb-post-name">';
+			$return .=  '<a href="'. get_permalink( $single_post->ID ) .'" rel="bookmark" title="'. get_the_title( $single_post->ID ) .'">'. get_the_title( $single_post->ID ) .'</a>';
+			$return .=  '</li>';
+		
+		endforeach;
+		
+		endif;
+	
+	$return .=  '</ul>';
+		
+	endforeach;
+	
+	$return .= '</div>';
+
+	return $return;
+	
+}
+
+function register_shortcodes(){
+	add_shortcode('knowledgebase', 'knowledgebase_function');
+}
+
+add_action( 'init', 'register_shortcodes');
+
+?>
